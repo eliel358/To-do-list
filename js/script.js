8f3210961f39037aa203ce90660a9a6e772a5398
eliel358 <92613589+eliel358@users.noreply.github.com>
2023-07-20 13:17:02 -0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-
 function create_task(){
     const taskn = document.createElement("div");
     const button = document.createElement("button");
     task_name = document.querySelector('input').value
+    task_name = '-'+task_name
+    if (task_name == ""){
+        return;
+    }
     taskn.innerHTML = task_name
     button.textContent = '❌'
 
@@ -11,9 +14,26 @@ function create_task(){
     taskn.id = task_name
     
     taskn.appendChild(button)
-    
     document.getElementById('tasks').appendChild(taskn)
-    button.onclick = function(){
-        document.getElementById('tasks').removeChild(taskn)
-    }
+    
+    
+    button.setAttribute('onclick',"rm('"+task_name+"')")
+    console.log(task_name)
 }
+
+
+
+function rm(task_name){
+    console.log(task_name)
+    console.log(tasks)
+    document.getElementById('tasks').removeChild(document.getElementById(task_name))
+}
+
+
+document.addEventListener('keypress',(event) => {
+    
+    if (event.code == 'Enter'){
+        create_task()
+    }
+
+})
