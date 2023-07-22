a7b544e4584f2ec89283a48336eb3ddae32f808c
eliel358 <92613589+eliel358@users.noreply.github.com>
2023-07-21 21:18:05 -0300

    adicionado salvamento automático em local storage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-var name = localStorage.getItem('nome')
+task_list = []
 
-
-function create_task(){
+function create_task(name){
     const new_task = document.createElement('div')
     const text = document.createElement('div')
     const buttons = document.createElement('div')
@@ -10,13 +9,8 @@ function create_task(){
     const bt_progress = document.createElement('div')
     const bt_remove = document.createElement('div')
     var tasks = document.getElementById('tasks')
-    var task_name = document.querySelector('input').value
-    
-    
+    var task_name = name
 
-    task_name = '-'+task_name
-    
-        
     tasks.appendChild(new_task)
     new_task.appendChild(text)
     new_task.appendChild(buttons)
@@ -24,7 +18,7 @@ function create_task(){
     buttons.appendChild(bt_check)
     buttons.appendChild(bt_not_check)
     buttons.appendChild(bt_remove)    
-    
+
     text.className = 'text'
     buttons.className = 'buttons'
     new_task.className = 'task'
@@ -40,20 +34,24 @@ function create_task(){
     bt_progress.innerHTML = '⌛'
     bt_remove.innerHTML = '⛔'
    
-    
     bt_progress.setAttribute('onclick',"progress('"+task_name+"')")
     bt_check.setAttribute('onclick',"check('"+task_name+"')")
     bt_not_check.setAttribute('onclick',"no_check('"+task_name+"')")
     bt_remove.setAttribute('onclick',"rm('"+task_name+"')")
     new_task.id = task_name
-
-
+ 
+    task_list = task_list.concat(task_name)
+    localStorage.setItem('tasks',JSON.stringify(task_list))
 }
 
 function rm(task_name){ 
     document.getElementById('tasks').removeChild(document.getElementById(task_name))
+    task_list = localStorage.getItem('tasks')
+    task_list = JSON.parse(task_list)
+    target_index = task_list.indexOf(task_name)
+    task_list.splice(target_index,1)
+    localStorage.setItem('tasks',JSON.stringify(task_list))
 }
-
 
 function no_check(task_name){
     text = document.getElementById(task_name).innerHTML
@@ -100,7 +98,13 @@ function progress(task_name){
 document.addEventListener('keypress',(event) => {
     
     if (event.code == 'Enter'){
-        create_task()
+        create_task(document.querySelector('input').value)
     }
 
 })
+task_l = localStorage.getItem('tasks')
+task_l = JSON.parse(task_l)
+
+task_l.forEach(task => {
+    create_task(task)
+});
