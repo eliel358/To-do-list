ef5158c3813e0babb6b945d08129d58850a2dd5a
eliel358 <elielhenrique360flip@gmail.com>
2024-01-17 00:01:01 -0300

    Concertando bug em que só era possivel deletar uma task

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 class task{
     constructor(task_name){
+
         this.task_name = task_name
         this.task = document.createElement('div')
-        this.task_p = document.createElement('p')
-        this.task_button = document.createElement('button')
         this.task.className = "task"
-        this.task_p.innerHTML = this.task_name
-        this.task_button.innerHTML = "Feito"
-        this.task.appendChild(this.task_p) 
-        this.task.appendChild(this.task_button)
-        document.getElementById("tasks-conteiner").appendChild(this.task)
-
         
+        this.task_button = document.createElement('button')
+        this.task_button.innerHTML = "Feito"
+        this.task_button.onclick = this.delete_task
+        this.task.appendChild(this.task_button)
+        
+        this.task_p = document.createElement('p')
+        this.task_p.innerHTML = this.task_name
+        this.task.appendChild(this.task_p) 
+
+        document.getElementById("tasks-conteiner").appendChild(this.task)
     } 
     delete_task = () => {
         this.task.remove()
@@ -19,5 +22,5 @@ class task{
 }
 document.getElementById("insert-button").addEventListener('click',function(){
     new_task = new task(document.getElementById('input-task-name').value)
-    new_task.task_button.addEventListener('click',function(){new_task.delete_task()})
+    
 })
